245305b5192d82d4b9f3df73439f1f20091542d7
Ian Johnson <lambdude@protonmail.com>
2018-03-15 22:01:04 -0400

    Update terraform configuration

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  region     = "us-east-1"
+  access_key     = "${var.access_key}"
+  secret_key     = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   # depends_on = ["example_bucket"]
 
